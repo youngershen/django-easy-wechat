755c13bf86606c727f0d6a7e9d60b6f36954d076
Younger Shen <younger.x.shen@gmail.com>
2019-03-02 10:58:16 +0800

    init

---
+++ src/dewechat/__init__.py
@@ -0,0 +1,9 @@
+# PROJECT : django-easy-wechat
+# TIME : 19-3-2 上午10:53
+# AUTHOR : Younger Shen
+# EMAIL : younger.x.shen@gmail.com
+# CELL : 13811754531
+# WECHAT : 13811754531
+# WEB : https://youngershen.com
+
+VERSION = '0.0.1'
---
+++ src/tests/__init__.py
@@ -0,0 +1,7 @@
+# PROJECT : django-easy-wechat
+# TIME : 19-3-2 上午10:53
+# AUTHOR : Younger Shen
+# EMAIL : younger.x.shen@gmail.com
+# CELL : 13811754531
+# WECHAT : 13811754531
+# WEB : https://youngershen.com
